0c2c1670c13df4cfcfae9cf602ecd3fe789d487d
Priyansh2116 <priyanshsonthalia23@gmail.com>
2024-09-13 18:40:39 +0530

    fix no key on link

--- app/page.tsx
+++ app/page.tsx
@@ -121,7 +121,7 @@ const Home = () => {
             { name: 'Life Science Domain', img: '/life-science-domain.png', link: '/life-science-domain' },
             { name: 'Corporate Domain', img: '/corporate-domain.png', link: '/corporate-domain' },
           ].map((member, index) => (
-            <a href={member.link} >
+            <a key={member.name} href={member.link} >
               <div className="bg-gray-800 p-4 sm:p-6 rounded-lg hover:bg-gray-700 hover:scale-105 transform transition-transform duration-300">
                 <img src={member.img} alt={member.name} className="w-24 sm:w-32 h-24 sm:h-32 mx-auto rounded-full" />
                 <h3 className="text-white text-sm sm:text-base lg:text-xl mt-2 sm:mt-4 font-mono">{member.name}</h3>
