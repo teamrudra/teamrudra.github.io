e8e1e956b0abcb85f55f92d9151af84dbd153fdc
Priyansh2116 <priyanshsonthalia23@gmail.com>
2024-09-14 22:25:47 +0530

    Navbar fix undo

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
-import 'tailwindcss/tailwind.css';
+
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
