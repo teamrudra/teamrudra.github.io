6ff29f3a579ae3778f0879c7d09039450872c77c
Priyansh2116 <priyanshsonthalia23@gmail.com>
2024-09-15 20:53:40 +0530

    Photo change

--- app/page.tsx
+++ app/page.tsx
@@ -174,7 +174,7 @@ const Home = () => {
         {/* Left Side: 3D Model */}
         <div className="w-full lg:w-3/5 flex justify-center items-center border-4 border-gray-300 box-border mb-8 lg:mb-0">
         <img
-      src="./your-image.png" // Update with the path to your image
+      src="./box-image.png" // Update with the path to your image
       alt="Rover model" // Update with a description for accessibility
       className="object-contain max-w-full max-h-full" // Adjust as needed
     />
