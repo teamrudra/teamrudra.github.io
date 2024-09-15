d134990249d1ba8261c5f27ca2fa0d201f11185c
Priyansh2116 <priyanshsonthalia23@gmail.com>
2024-09-15 14:21:11 +0530

    z axis 5

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,7 @@ const Home = () => {
       }
     `}</style>
           {/* Navbar */}
-          <nav className="bg-black bg-opacity-60 text-white fixed w-full z-100">
+          <nav className="bg-black bg-opacity-60 text-white fixed w-full z-200">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
         <Link href="/" legacyBehavior>
           <a>
