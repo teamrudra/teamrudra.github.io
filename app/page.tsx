4d606a278a2def67095284869d34fecf78b3b6d3
Priyansh2116 <priyanshsonthalia23@gmail.com>
2024-09-15 15:11:43 +0530

    navbar change

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,7 @@ const Home = () => {
       }
     `}</style>
           {/* Navbar */}
-          <nav className="bg-black bg-opacity-60 text-white fixed w-full z-50">
+          <nav className="bg-black bg-opacity-60 text-white fixed w-full z-10 transition-transform duration-300">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
         <Link href="/" legacyBehavior>
           <a>
@@ -151,15 +151,14 @@ const Home = () => {
     </nav>
 
 
-      {/* Hero Section */}
-      <div
-  className="relative min-h-screen bg-cover bg-center bg-no-repeat sm:bg-none z=60"
-  style={{
-    backgroundImage: `url('background-image.png')`, // Default background for larger screens
-  }}
-  id="home"
->
-  
+ {/* Hero Section */}
+ <div
+        className="relative min-h-screen bg-cover bg-center bg-no-repeat sm:bg-none z-20" // Adjusted z-index
+        style={{
+          backgroundImage: `url('background-image.png')`, // Default background for larger screens
+        }}
+        id="home"
+      >
   {/* <Navbar /> */}
   <div
     className="absolute inset-0 flex items-center justify-center sm:bg-cover sm:bg-center sm:bg-no-repeat"
