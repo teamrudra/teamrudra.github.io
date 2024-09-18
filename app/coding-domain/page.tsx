a2694edcc9f6917b469ce9271b9ede3d8c1c4d1d
Priyansh2116 <priyanshsonthalia23@gmail.com>
2024-09-18 21:48:47 +0530

    Ayush center

--- app/coding-domain/page.tsx
+++ app/coding-domain/page.tsx
@@ -9,13 +9,11 @@ const CodingDomainPage = () => {
       <SparklesCore className="absolute inset-0 z-0" particleColor="#ffffff" particleDensity={30} />
 
       <h2 className="text-white text-4xl mb-12 z-10 font-serif">Coding Domain Team</h2>
-      
-      {/* Team Members Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-10 text-center z-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 text-center z-10">
         
         {/* Coding Team Member 1 */}
         <Link href="/coding-team-member-1">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
             <img src="./coding-member1.png" alt="Coding Team Member 1" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Team Lead-Shashwat Ganesh</h3>
           </div>
@@ -23,7 +21,7 @@ const CodingDomainPage = () => {
 
         {/* Coding Team Member 2 */}
         <Link href="/coding-team-member-2">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
             <img src="./coding-member2.png" alt="Coding Team Member 2" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Developers Lead-Waseem Riaz</h3>
           </div>
@@ -31,7 +29,7 @@ const CodingDomainPage = () => {
 
         {/* Coding Team Member 3 */}
         <Link href="/coding-team-member-3">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
             <img src="./coding-member3.png" alt="Coding Team Member 3" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Simulation & Testing-Pritesh Agarwal</h3>
           </div>
@@ -39,24 +37,21 @@ const CodingDomainPage = () => {
 
         {/* Coding Team Member 4 */}
         <Link href="/coding-team-member-4">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
             <img src="./coding-member4.png" alt="Coding Team Member 4" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Yasharth Gupta</h3>
           </div>
         </Link>
 
-      </div>
-      
-      {/* Centered Last Team Member */}
-      <div className="flex justify-center mt-10">
+        {/* Coding Team Member 5 */}
         <Link href="/coding-team-member-5">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
             <img src="./coding-member5.png" alt="Coding Team Member 5" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Ayush Daga</h3>
           </div>
         </Link>
-      </div>
 
+      </div>
     </div>
   );
 };
