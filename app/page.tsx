d6b9faad52e047465277c3427fadff6920c484d5
Priyansh2116 <priyanshsonthalia23@gmail.com>
2024-09-21 10:04:25 +0530

    Add yt

--- app/page.tsx
+++ app/page.tsx
@@ -432,7 +432,6 @@ const Home = () => {
           <img src="linkedin-brands-solid.svg" alt="LinkedIn" className="w-10 h-10" />
           <span className="text-white text-xl">Youtube</span>
         </a>
-      <div className="flex flex-col space-y-4 mt-6">
         <a href="https://www.linkedin.com/company/srmrudra/mycompany/" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-3">
           <img src="linkedin-brands-solid.svg" alt="LinkedIn" className="w-10 h-10" />
           <span className="text-white text-xl">LinkedIn</span>
