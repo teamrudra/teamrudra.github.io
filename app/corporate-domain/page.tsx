af366abf797c47a06cb76d20185795072e6390b5
Priyansh2116 <priyanshsonthalia23@gmail.com>
2024-09-21 22:41:07 +0530

    Corp changes

--- app/corporate-domain/page.tsx
+++ app/corporate-domain/page.tsx
@@ -10,19 +10,13 @@ const CorporateDomainPage = () => {
       <h2 className="text-white text-4xl mb-12 z-10 font-serif">Corporate Domain Team</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 text-center z-10">
         
-        {/* Corporate Team Member 1 */}
-        <Link href="https://www.linkedin.com/in/sanskar-patil-96a82922a/">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
-            <img src="./corporate-member1.png" alt="Corporate Team Member 1" className="w-32 h-32 mx-auto rounded-full" />
-            <h3 className="text-white text-xl mt-4">Sponsorship Lead-Sanskar Patil</h3>
-          </div>
-        </Link>
+
 
         {/* Corporate Team Member 2 */}
         <Link href="https://www.linkedin.com/in/rishita-seetha-317626237/">
           <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
             <img src="./corporate-member2.png" alt="Corporate Team Member 2" className="w-32 h-32 mx-auto rounded-full" />
-            <h3 className="text-white text-xl mt-4">Outreach Lead-Rishitha Seetha</h3>
+            <h3 className="text-white text-xl mt-4">Outreach Lead-Rishita Seetha</h3>
           </div>
         </Link>
 
@@ -31,6 +25,13 @@ const CorporateDomainPage = () => {
           <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
             <img src="./corporate-member3.png" alt="Corporate Team Member 3" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Creatives Lead-Srishti Chatterjee</h3>
+          </div>
+        </Link>
+                {/* Corporate Team Member 1 */}
+                <Link href="https://www.linkedin.com/in/sanskar-patil-96a82922a/">
+          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
+            <img src="./corporate-member1.png" alt="Corporate Team Member 1" className="w-32 h-32 mx-auto rounded-full" />
+            <h3 className="text-white text-xl mt-4">Sponsorship Lead-Sanskar Patil</h3>
           </div>
         </Link>
 
