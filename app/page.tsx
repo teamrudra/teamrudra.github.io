36b403f9deaff675c4c8321181b9e974a12a894a
Shivi-013 <guptashivi080@gmail.com>
2024-11-16 18:38:37 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -472,31 +472,27 @@ const Home = () => {
     </div>
   </div>
 </div>
-
-      <div className="min-h-screen bg-black flex flex-col items-center justify-center py-16 relative" id="sponsor us">
+      <div className="min-h-screen bg-black flex flex-col items-center justify-center py-16 relative" id="newsletter">
   <SparklesCore className="absolute inset-0 z-0" particleColor="#f0f0f0" particleDensity={30} />
   <div className="bg-gray-900 p-8 rounded-2xl shadow-lg w-[1000px] h-[480px] max-w-full relative flex flex-row items-center">
     <div className="flex-1 mr-8">
       <img src="newsletter.jpeg" alt="Rover Image" className="w-[440px] h-[400px] object-cover rounded-xl" />
     </div>
     <div className="flex-1 text-center">
-      <h2 className="text-white text-3xl font-semibold mb-2">Support Team Rudra</h2>
-      <p className="text-gray-400 mb-6">If you're interested in supporting our Mars Rover project, please fill out the form below, and we’ll get in touch!</p>
+      <h2 className="text-white text-3xl font-semibold mb-2">Join Our Newsletter</h2>
+      <p className="text-gray-400 mb-6">Get weekly access to our newsletter and stay updated</p>
     <form action="https://script.google.com/macros/s/AKfycbx6TQFdNnrVyGVI-ThpC-3pxaZugmHMUcdDqp-S9c31o5LDkjiXuqr7xBwh5Uf8hYmP/exec" method="POST" className="space-y-4">
   <div className="relative">
     <input type="email" name="email" placeholder="Enter your email here *" className="w-full p-4 rounded-lg bg-gray-800 text-white border border-gray-700 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500" required />
   </div>
-      <div className="relative">
-    <input type="text" name="company" placeholder="Enter your organisation name " className="w-full p-4 rounded-lg bg-gray-800 text-white border border-gray-700 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500" required />
-  </div>
-  <button type="submit" className="w-full py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition">Submit</button>
+  <button type="submit" className="w-full py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition">Subscribe</button>
 </form>
 
 
     </div>
   </div>
 </div>
-      </div>
+  </div>
   
   );
 };
