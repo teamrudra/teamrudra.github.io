a048dac59df7b0e251f19b34d2549c9ae401e074
Priyansh2116 <priyanshsonthalia23@gmail.com>
2024-11-16 23:42:18 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
   event.preventDefault();
 
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbx6TQFdNnrVyGVI-ThpC-3pxaZugmHMUcdDqp-S9c31o5LDkjiXuqr7xBwh5Uf8hYmP/exec", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbzaVFUDPT1rTOg9BRgzbdZ_7aX2o8AvLc4wHDhWm_QDZQofvdGCBIh67xr8K7KThx8P/exec", {
       method: "POST",
       body: JSON.stringify({ email }),
       headers: {
@@ -486,7 +486,7 @@ const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     <div className="flex-1 text-center">
       <h2 className="text-white text-3xl font-semibold mb-2">Join Our Newsletter</h2>
       <p className="text-gray-400 mb-6">Get weekly access to our newsletter and stay updated</p>
-    <form action="https://script.google.com/macros/s/AKfycbx6TQFdNnrVyGVI-ThpC-3pxaZugmHMUcdDqp-S9c31o5LDkjiXuqr7xBwh5Uf8hYmP/exec" method="POST" className="space-y-4">
+    <form action="https://script.google.com/macros/s/AKfycbzaVFUDPT1rTOg9BRgzbdZ_7aX2o8AvLc4wHDhWm_QDZQofvdGCBIh67xr8K7KThx8P/exec" method="POST" className="space-y-4">
   <div className="relative">
     <input type="email" name="email" placeholder="Enter your email here *" className="w-full p-4 rounded-lg bg-gray-800 text-white border border-gray-700 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500" required />
   </div>
