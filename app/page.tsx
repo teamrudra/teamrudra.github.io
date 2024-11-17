1a8f3f49a1f9e3beb59e668253af6b94a50d651c
Shivi-013 <guptashivi080@gmail.com>
2024-11-18 00:53:56 +0530

    js-submit

--- app/page.tsx
+++ app/page.tsx
@@ -60,14 +60,10 @@ const form = document.forms['contact-form'];
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(response => {
-      if (response.ok) {
-        alert('Thank you! Your form is submitted successfully.');
-        form.reset(); // Reset form fields after successful submission
-      } else {
-        alert('Failed to submit. Please try again.');
-      }
-    })
+    .then(response => alert("Thank you! your form is submitted successfully."))
+    .then(() => { window.location.reload(); })
+    
+     
     .catch(error => console.error('Error!', error.message));
 });
 
