b413d3f8abf6b640372368e5e3b3e2b0a53cf1a8
Shivi-013 <guptashivi080@gmail.com>
2024-11-17 18:13:54 +0530

    url remove

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
 
   try {
     // Only one fetch statement here
-    const response = await fetch("https://script.google.com/macros/s/AKfycbzaVFUDPT1rTOg9BRgzbdZ_7aX2o8AvLc4wHDhWm_QDZQofvdGCBIh67xr8K7KThx8P/exec", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbxnxzbOk42QDZCoPQSGhVlczqRwhbd59SAV75T7IaV9Yz13Ud9zhzkToIvSyXrh3mbb5A/exec", {
       method: "POST",
       body: JSON.stringify({ email }), // Ensure `email` is defined
       headers: {
@@ -488,7 +488,7 @@ const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     <div className="flex-1 text-center">
       <h2 className="text-white text-3xl font-semibold mb-2">Join Our Newsletter</h2>
       <p className="text-gray-400 mb-6">Get weekly access to our newsletter and stay updated</p>
-    <form action="https://script.google.com/macros/s/AKfycbxnxzbOk42QDZCoPQSGhVlczqRwhbd59SAV75T7IaV9Yz13Ud9zhzkToIvSyXrh3mbb5A/exec" method="POST" className="space-y-4">
+    <form action="" method="POST" className="space-y-4">
   <div className="relative">
     <input type="email" name="email" placeholder="Enter your email here *" className="w-full p-4 rounded-lg bg-gray-800 text-white border border-gray-700 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500" required />
   </div>
