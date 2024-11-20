dfd9749505cdcde6fc7e9cd311a899d8e5fafa3b
Shivi-013 <guptashivi080@gmail.com>
2024-11-20 18:19:48 +0530

    danyal gems logo

--- app/page.tsx
+++ app/page.tsx
@@ -53,6 +53,7 @@ const Home = () => {
     { src: './vartech.png', alt: 'Var Tech' },
     { src: './protocase.png', alt: 'Protocase' },
     { src: './mathworks-logo-full-color-rgb-reversed.png' , alt: 'Mathworks' }
+      { src: './danyalgems.png' , alt: 'Danyal Gems' }
     
   ];
 const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
