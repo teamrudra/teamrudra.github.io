5f03bfcbb109ab696ab76840ebbc9892b0e7c137
Priyansh2116 <priyanshsonthalia23@gmail.com>
2024-12-04 16:59:49 +0530

    use environment variable for url

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,7 @@ const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     formData.append('email', email);
     
     const response = await fetch(
-      "https://script.google.com/macros/s/AKfycbw9sp49Ddtke7DZoflVuJYgIFdIGu2PXXI0BVLjYtfWbHvsRntUGiJCy-j_JAa0tgofiw/exec", 
+        process.env.NEXT_PUBLIC_GOOGLE_SCRIPT_URL as string, // Use the environment variable
       {
         method: "POST",
         body: formData
