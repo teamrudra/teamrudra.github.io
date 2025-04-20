1e0b3bd59f4b19cb5df48d164ea25795b410374f
Shivi-013 <guptashivi080@gmail.com>
2025-04-20 17:51:16 +0530

    Update corporate members

--- app/corporate-domain/page.tsx
+++ app/corporate-domain/page.tsx
@@ -13,49 +13,57 @@ const CorporateDomainPage = () => {
 
 
         {/* Corporate Team Member 2 */}
-        <Link href="https://www.linkedin.com/in/rishita-seetha-317626237/">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
-            <img src="./corporate-member2.png" alt="Corporate Team Member 2" className="w-32 h-32 mx-auto rounded-full" />
-            <h3 className="text-white text-xl mt-4">Outreach Lead-Rishita Seetha</h3>
-          </div>
-        </Link>
-
-        {/* Corporate Team Member 3 */}
-        <Link href="https://www.linkedin.com/in/srishti-chatterjee-72a82421b/">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
-            <img src="./corporate-member3.png" alt="Corporate Team Member 3" className="w-32 h-32 mx-auto rounded-full" />
-            <h3 className="text-white text-xl mt-4">Creatives Lead-Srishti Chatterjee</h3>
-          </div>
-        </Link>
-                {/* Corporate Team Member 1 */}
-                <Link href="https://www.linkedin.com/in/sanskar-patil-96a82922a/">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
-            <img src="./corporate-member1.png" alt="Corporate Team Member 1" className="w-32 h-32 mx-auto rounded-full" />
-            <h3 className="text-white text-xl mt-4">Sponsorship Lead-Sanskar Patil</h3>
-          </div>
-        </Link>
-
-        {/* Corporate Team Member 4 */}
         <Link href="https://www.linkedin.com/in/danyal-reyaz/">
           <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
-            <img src="./corporate-member4.png" alt="Corporate Team Member 4" className="w-32 h-32 mx-auto rounded-full" />
+            <img src="./danyal.JPG" alt="Corporate Team Member 2" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Danyal Reyaz</h3>
           </div>
         </Link>
 
+        {/* Corporate Team Member 3 */}
+        <Link href="https://www.linkedin.com/in/nidhan-santa-kumar-n/">
+          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
+            <img src="./corporate-member6.png" alt="Corporate Team Member 3" className="w-32 h-32 mx-auto rounded-full" />
+            <h3 className="text-white text-xl mt-4">Creatives Lead-Nidhan SKN</h3>
+          </div>
+        </Link>
+                {/* Corporate Team Member 1 */}
+                <Link href="https://www.linkedin.com/in/ditsa-sarkar-7a0623228/">
+          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
+            <img src="./corporate-member1.png" alt="Corporate Team Member 1" className="w-32 h-32 mx-auto rounded-full" />
+            <h3 className="text-white text-xl mt-4">Ditsa Sarkar</h3>
+          </div>
+        </Link>
+
+        {/* Corporate Team Member 4 */}
+        <Link href="">
+          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
+            <img src="./corporate-member4.png" alt="Corporate Team Member 4" className="w-32 h-32 mx-auto rounded-full" />
+            <h3 className="text-white text-xl mt-4">Bhargavi Singh</h3>
+          </div>
+        </Link>
+
         {/* Corporate Team Member 5 */}
-        <Link href="https://www.linkedin.com/in/priyansh-sonthalia-380817265/">
+        <Link href="https://www.linkedin.com/in/shivi-gupta-642651301/">
           <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
             <img src="./corporate-member5.png" alt="Corporate Team Member 5" className="w-32 h-32 mx-auto rounded-full" />
-            <h3 className="text-white text-xl mt-4">Priyansh Sonthalia</h3>
+            <h3 className="text-white text-xl mt-4">Shivi Gupta</h3>
           </div>
         </Link>
 
         {/* Corporate Team Member 6 */}
-        <Link href="https://www.linkedin.com/in/nidhan-santa-kumar-n/">
+        <Link href="https://www.linkedin.com/in/snehal-gupta-44a791222/">
           <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
             <img src="./corporate-member6.png" alt="Corporate Team Member 6" className="w-32 h-32 mx-auto rounded-full" />
-            <h3 className="text-white text-xl mt-4">Nidhan SKN</h3>
+            <h3 className="text-white text-xl mt-4">Snehal gupta</h3>
+          </div>
+        </Link>
+
+         {/* Corporate Team Member 7 */}
+        <Link href="https://www.linkedin.com/in/shriya-rao-4773352bb/">
+          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300 relative z-10">
+            <img src="./corporate-member5.png" alt="Corporate Team Member 5" className="w-32 h-32 mx-auto rounded-full" />
+            <h3 className="text-white text-xl mt-4">Shriya Rao</h3>
           </div>
         </Link>
         
