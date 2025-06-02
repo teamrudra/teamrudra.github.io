be38693b19fd833a2dd523145e5456ec454a355e
Shivi-013 <guptashivi080@gmail.com>
2025-06-02 11:27:06 +0530

    Update team2018 images

--- app/Team18/page.tsx
+++ app/Team18/page.tsx
@@ -4,28 +4,28 @@ import { SparklesCore } from '@/components/ui/sparkles';
 
 const Team18Page = () => {
   const teamMembers = [
-    { name: "Abhishek Patel", designation: "Team Lead", image: "abhishekp.png" },
-    { name: "Bhavansh Tandon", designation: "Team Manager & Electronics Lead", image: "bhavansh.png" },
-    { name: "Varun Ramesh", designation: "Technical Director", image: "varun.png" },
-    { name: "Sonali Sinha Roy", designation: "Mechanical Lead", image: "sonali.png" },
-    { name: "Vaibhav Shelke", designation: "Coding Lead", image: "vaibhav.png" },
-    { name: "Uthvag", designation: "Science Lead", image: "uthvag.png" },
-    { name: "Chirag Bharani", designation: "Sponsorship Lead", image: "chirag.png" },
-    { name: "Akash Sehgal", designation: "Marketing Lead", image: "aakash.png" },
-    { name: "Hari Krishna", designation: "Electronics Team", image: "hari.png" },
-    { name: "Maanik Narayan", designation: "Electronics Team", image: "maanik.png" },
-    { name: "Krishanu Kumar", designation: "Mechanical Team", image: "krishanu.png" },
-    { name: "Gauravjit Singh Gill", designation: "Mechanical Team", image: "gauravjit.png" },
-    { name: "Rahul R", designation: "Mechanical Team", image: "rahul.png" },
-    { name: "Lochan RN", designation: "Coding Team", image: "lochan.png" },
-    { name: "Kriti Pratap", designation: "Coding Team", image: "kriti.png" },
-    { name: "Nikil Datta", designation: "Corporate Team", image: "nikhil.png" },
-    { name: "Shri Venkatesh", designation: "Science Team", image: "shri.png" },
-    { name: "Rahul Agarwal", designation: "Mechanical Team", image: "rahulag.png" },
-    { name: "David Chaudhary", designation: "Mechanical Team", image: "david.png" },
-    { name: "Chaitanya Joshi", designation: "Mechanical Team", image: "chaitanya.png" },
-    { name: "Renish Kumar", designation: "Developer", image: "renish.png" },
-    { name: "Abhishek Reddy", designation: "Developer", image: "abhishekr.png" },
+   { name: "Abhishek Patel", designation: "Team Lead", image: "./abhishek.jpg" },
+  { name: "Bhavansh Tandon", designation: "Team Manager & Electronics Lead", image: "./bhavansh.jpeg" },
+  { name: "Varun Ramesh", designation: "Technical Director", image: "./varun.jpg" },
+  { name: "Sonali Sinha Roy", designation: "Mechanical Lead", image: "./sonali.jpg" },
+  { name: "Vaibhav Shelke", designation: "Coding Lead", image: "./vaibhav.jpeg" },
+  { name: "Uthvag", designation: "Science Lead", image: "./uthvag.jpeg" },
+  { name: "Chirag Bharani", designation: "Sponsorship Lead", image: "./chirag.jpeg" },
+  { name: "Akash Sehgal", designation: "Marketing Lead", image: "./akash.JPG" },
+  { name: "Hari Krishna", designation: "Electronics Team", image: "./hari.png" },
+  { name: "Maanik Narayan", designation: "Electronics Team", image: "./mani.JPG" },
+  { name: "Krishanu Kumar", designation: "Mechanical Team", image: "./krishanu.JPG" },
+  { name: "Gauravjit Singh Gill", designation: "Mechanical Team", image: "./gauravjit.jpg" },
+  { name: "Rahul R", designation: "Mechanical Team", image: "./rahul.jpg" },
+  { name: "Lochan RN", designation: "Coding Team", image: "./lochan.png" },
+  { name: "Kriti Pratap", designation: "Coding Team", image: "./kriti.JPG" },
+  { name: "Nikil Datta", designation: "Corporate Team", image: "./nikhil.webp" },
+  { name: "Shri Venkatesh", designation: "Science Team", image: "./shri.webp" },
+  { name: "Rahul Agarwal", designation: "Mechanical Team", image: "./rahulag.webp" },
+  { name: "David Chaudhary", designation: "Mechanical Team", image: "./david.webp" },
+  { name: "Chaitanya Joshi", designation: "Mechanical Team", image: "./chaitanya.webp" },
+  { name: "Renish Kumar", designation: "Developer", image: "./renish.JPG" },
+  { name: "Abhishek Reddy", designation: "Developer", image: "./abhishek.jpg" },     
   ];
 
   return (
@@ -34,7 +34,7 @@ const Team18Page = () => {
       {/* Sparkles Effect */}
       <SparklesCore className="absolute inset-0 z-0" particleColor="#ffffff" particleDensity={30} />
 
-      <h2 className="text-white text-4xl mb-12 z-10 font-serif">Team 18 Members</h2>
+      <h2 className="text-white text-4xl mb-12 z-10 font-serif">Team 2018 Members</h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 text-center z-10">
         {teamMembers.map((member, index) => (
