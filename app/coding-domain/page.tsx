17b9d92eff2239badf9ef8f1f84bcefa4ab418ea
Shivi-013 <guptashivi080@gmail.com>
2025-06-17 13:46:38 +0530

    Update coding

--- app/coding-domain/page.tsx
+++ app/coding-domain/page.tsx
@@ -4,7 +4,6 @@ import { SparklesCore } from '@/components/ui/sparkles';
 const CodingDomainPage = () => {
   return (
     <div className="min-h-screen bg-black flex flex-col items-center justify-center py-16 relative">
-      
       {/* Sparkles Effect */}
       <SparklesCore className="absolute inset-0 z-0" particleColor="#ffffff" particleDensity={30} />
 
@@ -14,7 +13,7 @@ const CodingDomainPage = () => {
 
         {/* Coding Team Member 1 – Ayush Daga */}
         <Link href="https://www.linkedin.com/in/ayush-daga-341045226/">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./coding-member5.png" alt="Ayush Daga" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Ayush Daga</h3>
           </div>
@@ -22,7 +21,7 @@ const CodingDomainPage = () => {
 
         {/* Coding Team Member 2 – Yasharth Gupta */}
         <Link href="https://www.linkedin.com/in/yasharth-gupta-53749a255/">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./coding-member4.png" alt="Yasharth Gupta" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Yasharth Gupta</h3>
           </div>
@@ -30,7 +29,7 @@ const CodingDomainPage = () => {
 
         {/* Coding Team Member 3 – Lakshay Chhabra */}
         <Link href="https://www.linkedin.com/in/lakshayyy15/">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./lakshay.jpeg" alt="Lakshay Chhabra" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Lakshay Chhabra</h3>
           </div>
@@ -38,7 +37,7 @@ const CodingDomainPage = () => {
 
         {/* Coding Team Member 4 – Pavithra CP */}
         <Link href="https://www.linkedin.com/in/pavithra-cp/">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./pavithra.jpeg" alt="Pavithra CP" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Pavithra CP</h3>
           </div>
@@ -46,7 +45,7 @@ const CodingDomainPage = () => {
 
         {/* Coding Team Member 5 – Nikhil CP */}
         <Link href="https://www.linkedin.com/in/nikhilcp05/">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./nikhil.jpeg" alt="Nikhil CP" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Nikhil CP</h3>
           </div>
@@ -54,7 +53,7 @@ const CodingDomainPage = () => {
 
         {/* Coding Team Member 6 – Rajrupa Das */}
         <Link href="https://www.linkedin.com/in/rajrupa-das-8b9597324/">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./rajrupa.jpeg" alt="Rajrupa Das" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Rajrupa Das</h3>
           </div>
@@ -62,7 +61,7 @@ const CodingDomainPage = () => {
 
         {/* Coding Team Member 7 – Aryan Garg */}
         <Link href="#">
-          <div className="bg-gray-800 p-6 rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./aryan.jpeg" alt="Aryan Garg" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Aryan Garg</h3>
           </div>
