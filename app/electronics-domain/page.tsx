4dcfc9db26f2bdf847266f46d138fea77d547c29
Shivi-013 <guptashivi080@gmail.com>
2025-06-18 11:45:59 +0530

    Update electronics

--- app/electronics-domain/page.tsx
+++ app/electronics-domain/page.tsx
@@ -19,7 +19,7 @@ const ElectronicsDomainPage = () => {
 
         {/* Technical Director – Pragati Prithvi */}
         <Link href="https://www.linkedin.com/in/pragati-prithvi/">
-          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-[300px] h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./electronics-member6.png" alt="Pragati Prithvi" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Technical Director - Pragati Prithvi</h3>
           </div>
@@ -27,63 +27,63 @@ const ElectronicsDomainPage = () => {
 
         {/* Electronics Lead – Sonakshi Srivastava */}
         <Link href="https://www.linkedin.com/in/srivastava-sonakshi/">
-          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-[300px] h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./electronics-member5.png" alt="Sonakshi Srivastava" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Electronics Lead - Sonakshi Srivastava</h3>
           </div>
         </Link>
 
-        {/* Member – Kaustubh Guha */}
+        {/* Kaustubh Guha */}
         <Link href="https://www.linkedin.com/in/kaustub-guha/">
-          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-[300px] h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./electronics-member4.png" alt="Kaustubh Guha" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Kaustubh Guha</h3>
           </div>
         </Link>
 
-        {/* Member – Sidhant Chakrabarti */}
+        {/* Sidhant Chakrabarti */}
         <Link href="https://www.linkedin.com/in/sidhant1226/">
-          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-[300px] h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./electronics-member7.png" alt="Sidhant Chakrabarti" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Sidhant Chakrabarti</h3>
           </div>
         </Link>
 
-        {/* Member – Aaryan Sarat */}
+        {/* Aaryan Sarat */}
         <Link href="https://in.linkedin.com/in/aaryan-sarat-491241331">
-          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-[300px] h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./aaryan.jpeg" alt="Aaryan Sarat" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Aaryan Sarat</h3>
           </div>
         </Link>
 
-        {/* Member – V Krishna */}
+        {/* V Krishna */}
         <Link href="https://www.linkedin.com/in/vkrishna89/">
-          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-[300px] h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./vkrishna.jpeg" alt="V Krishna" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">V Krishna</h3>
           </div>
         </Link>
 
-        {/* Member – Hariharan R */}
+        {/* Hariharan R */}
         <Link href="">
-          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-[300px] h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./hariharan.jpeg" alt="Hariharan R" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Hariharan R</h3>
           </div>
         </Link>
 
-        {/* Member – Mishti Bansal */}
+        {/* Mishti Bansal */}
         <Link href="">
-          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-[300px] h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./mishti.jpeg" alt="Mishti Bansal" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Mishti Bansal</h3>
           </div>
         </Link>
 
-        {/* Member – Abhijeet Koushal */}
+        {/* Abhijeet Koushal */}
         <Link href="https://www.linkedin.com/in/abhijeeth-koushal-addenki-800a18277/">
-          <div className="bg-gray-800 p-6 w-64 h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
+          <div className="bg-gray-800 p-6 w-[300px] h-auto mx-auto rounded-lg hover:bg-gray-700 transition-colors duration-300">
             <img src="./abhijeet.jpeg" alt="Abhijeet Koushal" className="w-32 h-32 mx-auto rounded-full" />
             <h3 className="text-white text-xl mt-4">Abhijeet Koushal</h3>
           </div>
